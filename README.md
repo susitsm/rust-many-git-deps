025507f2d2b87ba626632b4722d43c9b254d3a92
Matyas Susits <susits.matyas@gmail.com>
2025-09-06 16:34:16 +0200

    add Zgit=shallow-deps to the benchmarks

--- README.md
+++ README.md
@@ -7,26 +7,38 @@ Run `./benchmark.sh` to reproduce
 Starting "cargo update" benchmarks
 Uncached, serial, cargo 1.91.0-nightly (840b83a10 2025-07-30):
 
-real	0m22.676s
-user	0m10.982s
-sys	0m0.448s
+real	0m18.497s
+user	0m6.969s
+sys	0m0.435s
 
 Uncached, parallel:
 
-real	0m8.537s
-user	0m12.055s
-sys	0m1.004s
+real	0m6.372s
+user	0m7.343s
+sys	0m0.825s
 
 Cached, serial, cargo 1.91.0-nightly (840b83a10 2025-07-30):
 
-real	0m5.272s
-user	0m1.027s
-sys	0m0.161s
+real	0m4.445s
+user	0m0.707s
+sys	0m0.101s
 
 Cached, parallel:
 
-real	0m1.757s
-user	0m1.897s
-sys	0m0.358s
+real	0m1.036s
+user	0m1.103s
+sys	0m0.246s
+
+Uncached, shallow, serial, cargo 1.91.0-nightly (840b83a10 2025-07-30):
+
+real	0m9.295s
+user	0m1.116s
+sys	0m0.215s
+
+Uncached, shallow, parallel:
+
+real	0m1.521s
+user	0m1.479s
+sys	0m0.317s
 
 ```
--- benchmark.sh
+++ benchmark.sh
@@ -13,19 +13,37 @@ fi
 GIT_DB="$CARGO_HOME/git/db"
 
 REMOVE="$GIT_DB/hashbrown* $GIT_DB/syn* $GIT_DB/bitflags* $GIT_DB/quote* $GIT_DB/serde* $GIT_DB/json* $GIT_DB/tokio* $GIT_DB/reqwest* $GIT_DB/tracing* $GIT_DB/anyhow* $GIT_DB/thiserror* $GIT_DB/futures-rs* $GIT_DB/bytes*"
+GIT_DB="$CARGO_HOME/git/checkouts"
+REMOVE2="$GIT_DB/hashbrown* $GIT_DB/syn* $GIT_DB/bitflags* $GIT_DB/quote* $GIT_DB/serde* $GIT_DB/json* $GIT_DB/tokio* $GIT_DB/reqwest* $GIT_DB/tracing* $GIT_DB/anyhow* $GIT_DB/thiserror* $GIT_DB/futures-rs* $GIT_DB/bytes*"
+REMOVE_ALL="$REMOVE $REMOVE2"
 echo "Starting \"cargo update\" benchmarks"
 CARGO_VERSION=$(cargo --version)
-rm -rf $REMOVE
+
+rm -rf $REMOVE_ALL
 echo "Uncached, serial, $CARGO_VERSION:"
 time cargo update --quiet
 echo
-rm -rf $REMOVE
+
+rm -rf $REMOVE_ALL
 echo "Uncached, parallel:"
 time ./cargo/target/release/cargo update --quiet
 echo
+
 echo "Cached, serial, $CARGO_VERSION:"
 time cargo update --quiet
 echo
+
 echo "Cached, parallel:"
 time ./cargo/target/release/cargo update --quiet
 echo
+
+rm -rf $REMOVE_ALL
+echo "Uncached, shallow, serial, $CARGO_VERSION:"
+time cargo -Zgit=shallow-deps update --quiet
+echo
+
+rm -rf $REMOVE_ALL
+echo "Uncached, shallow, parallel:"
+time ./cargo/target/release/cargo -Zgit=shallow-deps update --quiet
+echo
+
